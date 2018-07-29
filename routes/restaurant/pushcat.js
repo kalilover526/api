81422c853135540f0a19f00d7c9c647f1918e18b
kalilover <rahulk.0526@gmail.com>
2018-07-29 12:11:16 +0530

    kam kro jake

--- routes/restaurant/pushcat.js
+++ routes/restaurant/pushcat.js
@@ -1,7 +1,8 @@
 var push=require('../../models/restaurant')
 var mongoose=require('mongoose')
 var cat;
-var cats,obj,flag=0;
+var cats,obj,flag=0,index;
+var tempvariant,tempitem,tempprice,tempimage;
 
 exports.pushcat=(req,res)=>{cat=req.body.catname;
    if(!req.body.id)
@@ -24,15 +25,41 @@ exports.pushcat=(req,res)=>{cat=req.body.catname;
                 msg:"no restaurant found"
                })
            }else{
-               cats=data.category
+               cats=resData.category
+            
                for(let i=0;i<cats.length;i++){
                    if(cats[i].catname==req.body.catname)
                    {
                        obj=cats[i];
                        flag=1
-                   }
+                       index=i
+                                       }
                }
                
+               if(flag==1){
+                obj.item.push(req.body.item);
+                obj.price.push(req.body.price);
+                obj.variant.push(req.body.variant);
+                obj.image.push(req.body.image)
+            
+                push.findOneAndUpdate({_id:req.body.id,'category.catname':req.body.catname},{$set:{'category.$':obj}},(err,data)=>{
+                    if(err){
+                        res.json({
+                            success:false,
+                            msg:"data not insetssed"
+                        })
+                    }else{
+                        res.json({
+                            success:true,
+                            msg:"Data Inssserted",
+                            data:data.category
+                        })
+                    }
+                })
+            }
+            else{
+                
+
                temp={
                    catname:cat,
                    item:req.body.item,
@@ -40,21 +67,28 @@ exports.pushcat=(req,res)=>{cat=req.body.catname;
                    variant:req.body.variant,
                    image:req.body.image
                }
-               push.findOneAndUpdate({_id:req.body.id},{$push:{category:temp},},(err,data)=>{
-                   if(err){
-                       res.json({
-                           success:false,
-                           msg:"data not inseted"
-                       })
-                   }else{
-                       res.json({
-                           success:true,
-                           msg:"Data Inserted",
-                           data:data
-                       })
-                   }
-               })
-           }
+               
+            push.findOneAndUpdate({_id:req.body.id},{$push:{category:temp}},(err,data)=>{
+                if(err){
+                    res.json({
+                        success:false,
+                        msg:"data not inseted"
+                    })
+                }else{
+                    res.json({
+                        success:true,
+                        msg:"Data Inserted",
+                        data:data.category
+                    })
+                }
+            })
+            }
+            
+              
+
+
+            }
+           
        })
    }
 }
